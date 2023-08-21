40cf1ab05c1c20abb8a1c7ac4dc949ede4e4eb3a
aamoos <aamoos@naver.com>
2023-08-21 23:22:59 +0900

    조회용 샘플 데이터 입력

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -34,12 +34,18 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch){
-        return em.createQuery("select o from Order o join o.member m" +
-                " where o.status = :status " +
-                " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
+        return em.createQuery("select o from Order o join o.member m", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
                 .setMaxResults(1000) //최대 1000건
                 .getResultList();
+
+//        return em.createQuery("select o from Order o join o.member m" +
+//                " where o.status = :status " +
+//                " and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000) //최대 1000건
+//                .getResultList();
     }
 }
